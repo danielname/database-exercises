9cf80d8c1c5c5bde0c64e1c1bdb2797249b76f74
Daniel Name <daniel.v.name@gmail.com>
2022-11-07 11:22:15 -0600

    tables exercise mostly complete

---
+++ albums_migration.sql
@@ -0,0 +1,8 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (id INT NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(15) NOT NULL ,
+    name VARCHAR(20) NOT NULL ,
+    release_date CHAR(4),
+    sales FLOAT,
+    genre VARCHAR(10))
