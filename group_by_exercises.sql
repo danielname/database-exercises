d3f9318910a5471081511c29b11fb69247b42cf7
Daniel Name <daniel.v.name@gmail.com>
2022-11-08 15:57:38 -0600

    i accidentally skipped #6 in group by, now solved

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -8,4 +8,6 @@ SELECT DISTINCT first_name, last_name FROM employees WHERE last_name REGEXP '^E.
 
 SELECT DISTINCT last_name FROM employees WHERE last_name REGEXP 'q[^u]|q$';
 
+SELECT DISTINCT count(*), last_name FROM employees WHERE last_name REGEXP 'q[^u]|q$' GROUP BY last_name;
+
 SELECT count(*), gender FROM employees WHERE first_name IN ('Irena','Vidya','Maya') GROUP BY gender;
