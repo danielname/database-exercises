f654b9b4f094d4cfc5c79a0d0098533bd46600e3
Daniel Name <daniel.v.name@gmail.com>
2022-11-08 11:36:03 -0600

    complete clauses lecture

--- clauses_lecture.sql
+++ clauses_lecture.sql
@@ -6,7 +6,9 @@ SELECT first_name FROM employees WHERE first_name REGEXP 'sus';
 
 SELECT emp_no, first_name, last_name FROM employees WHERE emp_no BETWEEN 10026 AND 10082;
 
-SELECT emp_no, first_name, last_name FROM employees WHERE emp_no BETWEEN 10026 AND 10082; last_name IN ('Herber', 'Dredge', 'Lipner', 'Baek');
+SELECT emp_no, first_name, last_name FROM employees WHERE emp_no BETWEEN 10026 AND 10082;
+
+SELECT emp_no, first_name, last_name FROM employees WHERE last_name IN ('Herber', 'Dredge', 'Lipner', 'Baek');
 
 SELECT emp_no, first_name, last_name FROM employees WHERE emp_no < 10026;
 
@@ -14,4 +16,12 @@ SELECT emp_no, first_name, last_name FROM employees
     WHERE emp_no < 20000 AND (last_name IN ('Herber', 'Baek')
     OR first_name = 'Shridhar');
 # can use parens to group conditions, just like in other languages. otherwise, reads from left to right
+# 'AND' also has priority over 'OR' for some reason, so parentheses can be used to ensure the order of operations that you want.
 
+SELECT * FROM salaries WHERE salary NOT BETWEEN 50000 AND 100000;
+
+SELECT birth_date FROM employees WHERE birth_date NOT REGEXP '^195';
+
+SELECT birth_date FROM employees
+    WHERE birth_date NOT REGEXP '^195'
+    AND hire_date NOT LIKE '199%';
