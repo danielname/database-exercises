01790b1d143e9b4284d417ed039cb1e6b01a014a
Daniel Name <daniel.v.name@gmail.com>
2022-11-08 11:49:47 -0600

    where exercises part 1 complete

---
+++ where_exercises.sql
@@ -0,0 +1,7 @@
+USE employees;
+
+SELECT emp_no, first_name, last_name FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
+
+SELECT emp_no, first_name, last_name FROM employees WHERE last_name LIKE 'E%';
+
+SELECT emp_no, first_name, last_name FROM employees WHERE last_name REGEXP 'q';
