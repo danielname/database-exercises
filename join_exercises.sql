fa0d8f71d196dd2302b3ed914539eed64aa856a8
Daniel Name <daniel.v.name@gmail.com>
2022-11-09 13:47:51 -0600

    completed join women question

--- join_exercises.sql
+++ join_exercises.sql
@@ -6,3 +6,11 @@ SELECT dept_name AS 'Department',CONCAT(first_name, ' ', last_name) AS 'Departme
     JOIN departments d
         ON d.dept_no = m.dept_no
     WHERE m.to_date REGEXP '^9';
+
+SELECT dept_name AS 'Department',CONCAT(first_name, ' ', last_name) AS 'Department Manager'
+FROM dept_manager m
+         JOIN employees e
+              ON m.emp_no = e.emp_no
+         JOIN departments d
+              ON d.dept_no = m.dept_no
+WHERE m.to_date REGEXP '^9' AND e.gender = 'F';
