82396e11ba502dc120f353f1002ce8ee7e03540d
Daniel Name <daniel.v.name@gmail.com>
2022-11-10 10:31:44 -0600

    completed subqueries question #2 with and without subqueries

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,8 +1,21 @@
 USE employees;
 
 SELECT CONCAT(first_name, ' ', last_name)
-FROM employees
-WHERE hire_date =
-      (SELECT hire_date
-       FROM employees
-       WHERE emp_no = 101010);
+    FROM employees
+    WHERE hire_date =
+          (SELECT hire_date
+           FROM employees
+           WHERE emp_no = 101010);
+
+SELECT title
+FROM titles
+JOIN employees e
+    ON e.emp_no = titles.emp_no
+WHERE first_name = 'Aamod';
+
+SELECT title
+FROM titles
+WHERE emp_no IN
+      (SELECT emp_no
+          FROM employees
+          WHERE first_name = 'Aamod');
